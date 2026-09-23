686b3cb6c4c1daa71b7542ad1462c55c5083946a
anna-minervae <abenbow2@illinois.edu>
2026-09-23 03:25:10 -0500

    Most main sections finished

--- sources.md
+++ sources.md
@@ -10,3 +10,12 @@ https://www.reddit.com/r/learnjavascript/comments/1r9xv91/change_the_style_of_al
 Background image:
 https://www.w3schools.com/cssref/pr_background-image.php
 https://www.w3schools.com/cssref/pr_background-attachment.php
+
+https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+
+Videos:
+https://www.w3schools.com/html/html5_video.asp
+https://www.w3schools.com/html/html_youtube.asp
+
+Smooth Scrolling:
+https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollBy
--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,16 @@
 // import main from 'src/css/main.scss';
 
 window.onscroll = function() {resizeNavbar()};
+document.getElementById("carousel-left").onclick = function(){ carousel(-1)};
+document.getElementById("carousel-right").onclick = function(){ carousel(1)};
+
+document.getElementById("rep-button").onclick = function(){ republic()};
+document.getElementById("prin-button").onclick = function(){ principate()};
+document.getElementById("dom-button").onclick = function(){ dominate()};
+
+var current_slide = 0;
 
 function resizeNavbar() {
-    console.log(`scrolling...`);
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         var new_height = Math.min(60, 100 - (document.body.scrollTop - 100));
         document.getElementById("navbar").style.height = `${new_height}px`;
@@ -23,4 +30,81 @@ function resizeNavbar() {
             button.style.padding = "15px";
         }
     }
+
+    // handle buttons
+    var republic_Y = document.getElementById("republic-section").getBoundingClientRect().top - document.body.getBoundingClientRect().top - 50;
+    var princeps_Y = document.getElementById("principate-section").getBoundingClientRect().top - document.body.getBoundingClientRect().top;
+    var dominate_Y = (document.getElementById("dominate-section").getBoundingClientRect().top - 50) - document.body.getBoundingClientRect().top;
+    if (window.scrollY >= republic_Y && window.scrollY < princeps_Y) {
+        document.getElementById("rep-button").style.backgroundColor = "rgb(135, 33, 33)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(96, 18, 18)";
+    } else if (window.scrollY >= princeps_Y && window.scrollY < dominate_Y ) {
+        document.getElementById("rep-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(135, 33, 33)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(96, 18, 18)";
+    } else if (window.scrollY >= dominate_Y) {
+        document.getElementById("rep-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(135, 33, 33)";
+    } 
+    else {
+        document.getElementById("rep-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(96, 18, 18)";
+    }
+}
+
+function carousel(change) {
+    current_slide += change;
+    if (current_slide == 0 || current_slide > 2) {
+        current_slide = 0;
+        document.getElementById("carousel-main").innerHTML = "<p>According to legend, Romulus founded Rome around 770 BC after killing his twin brother Remus. Rome actually began as a monarchy. There were 7 kings: Romulus, Numa Pompilius, Tullus Hostilius, Ancus Marcius, Lucius Tarquinius Priscus, Servius Tullius, and Lucius Tarquinius Superbus. The monarchy was increasingly unpopular, and Tarquinius Superbus was overthrown in 509 BC after one of his sons assaulted a woman named Lucretia. Lucretia's husband, Collatinus, and Tarquinius Superbus' nephew, Brutus, became the first consuls of Rome.</p>"
+    } else if (current_slide == 1) {
+        document.getElementById("carousel-main").innerHTML = "<p>The Roman Republic was governed by the Senate, which entirely consisted of upperclass patrician men. However, following the Secession of the Plebs, a Tribune of the Plebs was elected to represent the lower class plebeians. Two consuls were elected each year to jointly lead Rome.</p>"
+    } else {
+        current_slide = 2;
+        document.getElementById("carousel-main").innerHTML = "<iframe width=\"960\" height=\"540\" src=\"https://www.youtube.com/embed/dEMW0CMyv8Q\"></iframe>";
+    }
+}
+
+function republic() {
+    var republic_Y = document.getElementById("republic-section").getBoundingClientRect().top - document.body.getBoundingClientRect().top;
+    
+    window.scrollBy({
+        top: document.getElementById("republic-section").getBoundingClientRect().top - 50,
+        left: 0,
+        behavior: "smooth",
+    });
+    
+
+    document.getElementById("rep-button").style.backgroundColor = "rgb(135, 33, 33)";
+    document.getElementById("prin-button").style.backgroundColor = "rgb(96, 18, 18)";
+    document.getElementById("dom-button").style.backgroundColor = "rgb(96, 18, 18)";
+}
+
+function principate() {
+    var princeps_Y = document.getElementById("principate-section").getBoundingClientRect().top - document.body.getBoundingClientRect().top + 20;
+    window.scrollBy({
+        top: document.getElementById("principate-section").getBoundingClientRect().top - 50,
+        left: 0,
+        behavior: "smooth",
+    });
+
+    document.getElementById("rep-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(135, 33, 33)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(96, 18, 18)";
+}
+
+function dominate() {
+    var dominate_Y = (document.getElementById("dominate-section").getBoundingClientRect().top - 50) - document.body.getBoundingClientRect().top;
+    window.scrollBy({
+        top: document.getElementById("dominate-section").getBoundingClientRect().top - 50,
+        left: 0,
+        behavior: "smooth",
+    });
+
+    document.getElementById("rep-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("prin-button").style.backgroundColor = "rgb(96, 18, 18)";
+        document.getElementById("dom-button").style.backgroundColor = "rgb(135, 33, 33)";
 }
